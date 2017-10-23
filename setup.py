0c4d11a106c2163f222886c6ebdaf90897a0be15
Qicheng Lao <qicheng.lao@gmail.com>
2017-10-22 23:58:34 -0400

    Update project setup

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
-from distutils.core import setup
+from os import path
+from setuptools import find_packages, setup
+
+here = path.abspath(path.dirname(__file__))
+
+with open('requirements.txt') as f:
+    requirements = f.read().split()
+
+with open(path.join(here, 'VERSION')) as f:
+    version = f.read().strip()
 
 setup(
-    name='basis',
-    version='',
-    packages=[''],
+    name='DeepOrg',
+    version=version,
     url='',
     license='',
-    author='qicheng',
+    author='Qicheng Lao',
     author_email='qicheng.lao@gmail.com',
-    description=''
+    description='Deep learning project organizer',
+    packages=find_packages(
+        exclude=['tests'],
+    ),
+    install_requires=requirements,
+    include_package_data=True,
 )
