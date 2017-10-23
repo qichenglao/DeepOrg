50097a00ec02bb807097fe6db416e6217df7acc3
Qicheng Lao <qicheng.lao@gmail.com>
2017-10-23 14:54:20 -0400

    Add batch size in evaluation

--- DeepOrg/helpers/evaluation.py
+++ DeepOrg/helpers/evaluation.py
@@ -2,9 +2,9 @@ from keras.utils import np_utils
 from sklearn.metrics import classification_report, confusion_matrix
 
 
-def evaluate(X_test, y_test, nb_classes, model, logging, logging_msg):
+def evaluate(X_test, y_test, nb_classes, model, batch_size, logging, logging_msg):
     # pred = np.empty(y_test.shape[0], dtype="uint32")
-    pred = model.predict(X_test).argmax(axis=-1)
+    pred = model.predict(X_test, batch_size=batch_size).argmax(axis=-1)
     # pred = model.predict_generator(data_test_generator(), steps=X_test.shape[0] // batch_size)
 
     logging.info(pred)
@@ -15,7 +15,7 @@ def evaluate(X_test, y_test, nb_classes, model, logging, logging_msg):
     Y_test = np_utils.to_categorical(y_test, nb_classes)
     PRED = np_utils.to_categorical(pred, nb_classes)
 
-    score = model.evaluate(X_test, Y_test)
+    score = model.evaluate(X_test, Y_test, batch_size=batch_size)
     # score = model.evaluate_generator(data_test_generator(), steps=X_test.shape[0] // batch_size)
     logging.info(score)
     logging_msg.info(classification_report(Y_test, PRED))
