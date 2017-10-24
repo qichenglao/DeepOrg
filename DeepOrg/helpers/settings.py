402c0770a662ee5221c174840b04747e21dd96cd
Qicheng Lao <qicheng.lao@gmail.com>
2017-10-24 17:09:35 -0400

    Add data generator without rotation

--- DeepOrg/helpers/settings.py
+++ DeepOrg/helpers/settings.py
@@ -24,6 +24,29 @@ def get_datagen():
     return datagen_train, datagen_val
 
 
+def get_datagen_no_rotation():
+    datagen_train = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=True,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=True,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
+        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=True)  # randomly flip images
+
+    datagen_val = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=True,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=True,  # divide each input by its std
+    )
+
+    return datagen_train, datagen_val
+
+
 def get_datagen_no_norm():
     datagen_train = ImageDataGenerator(
         featurewise_center=False,  # set input mean to 0 over the dataset
