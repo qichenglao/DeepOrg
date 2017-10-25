adab8374a2d657bb92952e1ac09499d041f5f5e0
Qicheng Lao <qicheng.lao@gmail.com>
2017-10-25 16:01:39 -0400

    Fix bug in file manager

--- DeepOrg/helpers/file_manager.py
+++ DeepOrg/helpers/file_manager.py
@@ -3,19 +3,19 @@ import os
 from DeepOrg.tools import alerts
 
 
-def init(save_result_path, model_name):
+def init(save_result_path, save_file_name):
     if not os.path.exists(save_result_path):
         os.mkdir(save_result_path)
 
-    if os.path.exists(os.path.join(save_result_path, model_name + '_log.txt')):
+    if os.path.exists(save_file_name + '_log.txt'):
         choice = alerts.show_delete(
             'Log file exists for this model. hdf5 and others may also exist. Do you want to delete previous ones?'
         )
         if choice:
             # delete all previous files
+            # make sure all model names are inside '()', as unique IDs
             for file in os.listdir(save_result_path):
-                # todo: remove files more carefully
-                if file.startswith(model_name):
+                if file.split(')')[0] == os.path.basename(save_file_name).split(')')[0]:
                     os.remove(os.path.join(save_result_path, file))
         else:
             exit(0)
--- DeepOrg/training.py
+++ DeepOrg/training.py
@@ -11,16 +11,19 @@ def train(model, model_name, optimizer, loss, save_file_name, data_generator_tra
     # we need to recompile the model for these modifications to take effect
     model.compile(loss=loss, optimizer=optimizer, metrics=['accuracy'])
 
-    early_stopper = EarlyStopping(monitor='val_acc', min_delta=0.001, patience=10)
+    # early_stopper = EarlyStopping(monitor='val_acc', min_delta=0.001, patience=10)
     csv_logger = CSVLogger(save_file_name + '.csv')
     model_checkpoint = ModelCheckpoint(save_file_name + '.hdf5', monitor='val_loss', save_best_only=True)
 
     logging.info('################################')
-    logging.info('training starts...')
+    if 'top' in save_file_name:
+        logging.info('top training starts...')
+    else:
+        logging.info('training starts...')
     logging.info('################################')
 
     history = model.fit_generator(data_generator_train, steps_per_epoch=total_train_size // batch_size, epochs=nb_epoch, verbose=1,
                                   validation_data=data_generator_val, validation_steps=total_val_size // batch_size,
-                                  callbacks=[csv_logger, early_stopper, model_checkpoint])
+                                  callbacks=[csv_logger, model_checkpoint])
 
     return history
