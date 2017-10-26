9833e5eb2abe099b3c0b9360c3696f341b44892a
Qicheng Lao <qicheng.lao@gmail.com>
2017-10-26 15:27:16 -0400

    Add back early stop

--- DeepOrg/training.py
+++ DeepOrg/training.py
@@ -11,7 +11,7 @@ def train(model, model_name, optimizer, loss, save_file_name, data_generator_tra
     # we need to recompile the model for these modifications to take effect
     model.compile(loss=loss, optimizer=optimizer, metrics=['accuracy'])
 
-    # early_stopper = EarlyStopping(monitor='val_acc', min_delta=0.001, patience=10)
+    early_stopper = EarlyStopping(monitor='val_acc', min_delta=0.001, patience=10)
     csv_logger = CSVLogger(save_file_name + '.csv')
     model_checkpoint = ModelCheckpoint(save_file_name + '.hdf5', monitor='val_loss', save_best_only=True)
 
@@ -24,6 +24,6 @@ def train(model, model_name, optimizer, loss, save_file_name, data_generator_tra
 
     history = model.fit_generator(data_generator_train, steps_per_epoch=total_train_size // batch_size, epochs=nb_epoch, verbose=1,
                                   validation_data=data_generator_val, validation_steps=total_val_size // batch_size,
-                                  callbacks=[csv_logger, model_checkpoint])
+                                  callbacks=[csv_logger, early_stopper, model_checkpoint])
 
     return history
